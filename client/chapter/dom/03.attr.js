6933fb580ac4c193c7ff847845989403f50c9149
dallae28 <3574214@naver.com>
2023-01-11 09:37:10 +0900

    dataset 분리 작업

--- client/chapter/dom/03.attr.js
+++ client/chapter/dom/03.attr.js
@@ -92,13 +92,23 @@ function setAttr(node,prop,value){
   // validation : 확인 
   if(typeof node === 'string') node = getNode(node);
   if(typeof prop !== 'string') throw new TypeError('setAttr 함수의 두 번째 인자는 문자 타입 이어야 합니다.')
+  
+  if(prop.includes('data')){
+    let rest = prop.slice(5);
+    node.dataset[rest] = value;
+  }
+
   if(!value) throw new SyntaxError('setAttr 함수의 세 번째 인자는 필수값입니다.')
 
   node.setAttribute(prop,value);
 
 }
 
+// ★ first 노드 안에 data-value의 값으로 hello를 지정하겠다.
 setAttr('.first','data-value','hello');
+
+
+attr()
 
 
 console.assert( getAttr('.first','class') === 'first' );
