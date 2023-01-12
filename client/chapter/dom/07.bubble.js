410f135325053fb18b8632a066900130b43c282d
dallae28 <3574214@naver.com>
2023-01-12 09:38:32 +0900

    [추가] 버블링

--- client/chapter/dom/07.bubble.js
+++ client/chapter/dom/07.bubble.js
@@ -5,6 +5,36 @@
 
 /* 버블링 ----------------------------------------------------------------- */
 
-c
+const visual = getNode('.visual');
+const news = getNode('.news');
+const desc = getNode('.desc');
+
+
+visual.addEventListener('click',(e) => {
+  let elem = e.currentTarget;
+    // console.log('target : ',e.target);
+    // console.log('currentTarget :  ' ,e.currentTarget);
+    // console.log(this === e.currentTarget);
+    // console.log(this);
+    console.log('%c visual','background:dodgerblue');
+
+    css('.pop','display','block');
+})
+
+getNode('.pop').addEventListener('click',(e)=>{
+  e.stopPropagation()
+  css('.pop','display','none');
+})
+
+/* 
+news.addEventListener('click',function(){
+    console.log('%c news','background:orange');
+})
+desc.addEventListener('click',function(e){
+    e.stopPropagation()
+    console.log('%c desc','background:hotpink');
+})
+ */
+
 
 /* 캡처링 ----------------------------------------------------------------- */
