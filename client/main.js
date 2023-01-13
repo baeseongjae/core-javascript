2fb4a706809db02077827b2485a5cfe8c02b1d0c
dallae28 <3574214@naver.com>
2023-01-13 13:13:33 +0900

    계산기 토이프로젝트 만들기

--- client/main.js
+++ client/main.js
@@ -1,1 +1,25 @@
-console.log('aa');
+/* eslint  no-undef: 'warn'  => eslint 끄는법*/
+
+// 1. input 값 가져오기
+// 2. 이벤트 핸들러 연결하기
+// 3. 이벤트 기본동작 차단하기
+// 4. 두 수의 합을 더해주기
+// 5. 화면에 출력하기
+
+const firstNum = document.querySelector("#firstNumber");
+const secondNum = document.querySelector("#secondNumber");
+const done = document.querySelector("#done");
+const result = document.querySelector(".result");
+const form = document.querySelector(".calculator");
+function calculator(e){
+  e.preventDefault();
+  
+  const firstValue = +firstNum.value;
+  const secondValue = +secondNum.value;
+  
+
+  result.innerHTML = `${firstValue+secondValue}`;
+}
+
+
+form.addEventListener('submit',calculator);
