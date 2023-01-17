b3f18165eb01ff63523b9ef4613b198931b8742f
dallae28 <3574214@naver.com>
2023-01-17 15:24:47 +0900

    [추가] xhr 구조 분해 할당

--- client/lib/utils/xhr.js
+++ client/lib/utils/xhr.js
@@ -11,24 +11,51 @@
   // xhrData함수만들기   method, url
 
 
-function xhrData(method, url){
+function xhrData(method, url, body){
   const xhr = new XMLHttpRequest();
   // 비동기 통신 오픈
   xhr.open(method,url);
   xhr.addEventListener('readystatechange',()=>{
-    if(xhr.status>=200 && xhr.status<400){
-      if(xhr.readyState===4)
-        console.log('통신성공');
+    const {status,readyState,response} = xhr; // 객체 구조 분해 할당 
+
+    if(status >= 200 && status < 400){
+      if(readyState === 4){
+        console.log('통신 성공');
+        console.log(JSON.parse(response));
+      }
     }else{
-      console.log('통신실패');
+      console.error('통신실패');
     }
   })
 
   // 서버에 요청
-  xhr.send();
+  xhr.send(JSON.stringify(body));
 }
 
-xhrData('GET','https://jsonplaceholder.typicode.com/users');
+/* GET
+ xhrData('GET','https://jsonplaceholder.typicode.com/users'); */
 
-
+// POST
+xhrData('POST','https://jsonplaceholder.typicode.com/users',{
+  "name": "kindtiger",
+  "username": "seonbeom",
+  "email": "tiger@euid.dev",
+  "address": {
+    "street": "Kulas Light",
+    "suite": "Apt. 556",
+    "city": "Gwenborough",
+    "zipcode": "92998-3874",
+    "geo": {
+      "lat": "-37.3159",
+      "lng": "81.1496"
+    }
+  },
+  "phone": "010-7169-0262",
+  "website": "hildegard.org",
+  "company": {
+    "name": "Romaguera-Crona",
+    "catchPhrase": "Multi-layered client-server neural-net",
+    "bs": "harness real-time e-markets"
+  }
+})
 
